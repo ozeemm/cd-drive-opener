f19234484bb8b35e35c746c823c1b25d473e32a5
ozeemm <77226278+ozeemm@users.noreply.github.com>
2023-10-18 01:00:08 +0800

    Added buttons and fun mode

--- Играться Дисковод.py
+++ Играться Дисковод.py
@@ -1,23 +1,51 @@
+from Tkinter import *
+from time import sleep
 import ctypes
+
 def opencd():
     ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0, None)
     
 def closecd():
     ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door closed", None, 0, None)
 
-print('Р§С‚Рѕ РґРµР»Р°С‚СЊ?')
-print('0. Р—Р°РєСЂС‹С‚СЊ Рё Р·Р°РЅРёРјР°С‚СЊСЃСЏ Р°РЅРіР»РёР№СЃРєРёРј')
-print('1. РћС‚РєСЂС‹С‚СЊ РґРёСЃРєРѕРІРѕРґ')
-print('2. Р—Р°РєСЂС‹С‚СЊ РґРёСЃРєРѕРІРѕРґ')
+def textMenu():
+    print('Что делать?')
+    print('0. Закрыть и заниматься английским')
+    print('1. Открыть дисковод')
+    print('2. Закрыть дисковод')
 
-while True:
-    todo = int(input('Р”РµР»Р°Р№С‚Рµ РІС‹Р±РѕСЂ: '))
-    if(todo == 1):
+    while True:
+        todo = int(input('Делайте выбор: '))
+        if(todo == 1):
+            opencd()
+        elif(todo == 2):
+            closecd()
+        else:
+            print('Ой, ну и не надо')
+            exit()
+
+def buttonsMenu():
+    root = Tk()
+    root.title('CD Drive opener')
+    root.configure(bg='gray')
+
+    open_button = Button(root, text="Open", height= 3, width=20, command=opencd)
+    open_button.pack()
+    close_button = Button(root, text="Close", height= 3, width=20, command=closecd)
+    close_button.pack()
+    fun_button = Button(root, text="FUN", height= 4, width=25, bg='#54FA9B', command=makeFun)
+    fun_button.pack()
+    exit_button = Button(root, text="Exit", height= 2, width=15, command=exit)
+    exit_button.pack()
+
+    root.mainloop()
+
+def makeFun():
+    while True:
         opencd()
-    elif(todo == 2):
+        sleep(2)
         closecd()
-    else:
-        print('РћР№, РЅСѓ Рё РЅРµ РЅР°РґРѕ')
-        exit()
-    
+        sleep(2)
+
+buttonsMenu()
     
